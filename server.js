c71ccf31c0467e2423ecdf2a1492889b925d85ea
Andrew Diaz <andrew.matt.diaz@gmail.com>
2016-11-07 14:56:27 -0500

    create server

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const isDev = !('NODE_ENV' in process.env) && require('dotenv').config() && true;
+
+const app = express();
+const port = process.argv[2] || process.env.PORT || 3000;
+
+app.listen(port, () => console.log('listening on port ', port));
+
+app.use(logger(isDev ? 'dev' : 'common'));
+app.use(bodyParser.json());
+
+app.use((err, req, res, next) => {
+  console.error(err, next);
+  res.status(500).send('soomethings broken');
+});
+
