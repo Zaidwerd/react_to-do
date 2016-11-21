b1e08c10b27b5b93ce872942e64136f44829099f
Andrew Diaz <andrew.matt.diaz@gmail.com>
2016-11-21 10:47:24 -0500

    student-solution3 branch solution

--- model/task.js
+++ model/task.js
@@ -1,11 +1,11 @@
 const pg = require('pg-promise')({/* OPTIONAL Initialization Options */});
 
 const config = {
-  host:       process.env.DB_HOST,
-  port:       process.env.DB_PORT,
-  database:   process.env.DB_NAME,
-  user:       process.env.DB_USER,
-  password:   process.env.DB_PASS,
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASS,
 };
 
 const db = pg(config);
